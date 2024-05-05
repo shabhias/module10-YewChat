7f33ec6539031b7364eff69e5cf4a8d927f495ee
shabhias <shabhialiyya3101@gmail.com>
2024-05-05 20:20:55 +0700

    components - phase 1

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,8 @@
 #![recursion_limit = "512"]
 
+use std::cell::RefCell;
+use std::rc::Rc;
+
 use wasm_bindgen::prelude::*;
 use yew::functional::*;
 use yew::prelude::*;
@@ -28,22 +31,36 @@ pub enum Route {
     NotFound,
 }
 
+pub type User = Rc<UserInner>;
+
+#[derive(Debug, PartialEq)]
+pub struct UserInner {
+    pub username: RefCell<String>,
+}
+
+#[function_component(Main)]
+fn main() -> Html {
+    let ctx = use_state(|| {
+        Rc::new(UserInner {
+            username: RefCell::new("initial".into()),
+        })
+    });
+    html! {
+        <ContextProvider<User> context={(*ctx).clone()}>
+            <BrowserRouter>
+                <div class="flex w-screen h-screen">
+                    <Switch<Route> render={Switch::render(switch)}/>
+                </div>
+            </BrowserRouter>
+        </ContextProvider<User>>
+    }
+}
+
 fn switch(selected_route: &Route) -> Html {
     match selected_route {
         Route::Login => html! {<Login />},
         Route::Chat => html! {<Chat/>},
         Route::NotFound => html! {<h1>{"404 baby"}</h1>},
-    }
-}
-
-#[function_component(Main)]
-fn main() -> Html {
-    html! {
-        <BrowserRouter>
-            <div class="flex w-screen h-screen">
-                <Switch<Route> render={Switch::render(switch)}/>
-            </div>
-        </BrowserRouter>
     }
 }
 
